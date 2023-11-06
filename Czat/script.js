9023bc16e9a8f7d4c7bfd52f47267b403258ce05
Traczini <traczini@gmail.com>
2023-11-06 17:00:21 +0100

    localSroage Czat

---
+++ Czat/script.js
@@ -0,0 +1,169 @@
+const $authorErrorMessage = document.getElementById('author-error');
+const $authorInput = document.getElementById('author');
+
+const $messageErrorMessage = document.getElementById('message-error');
+const $messageInput = document.getElementById('message');
+
+const $messageList = document.getElementById('message-list');
+
+const $reset = document.getElementById('btn-reset');
+
+
+// Ustawianie domyslnej wartosci przy uzyciu operatora || lub ??
+// const persistedMessagesString = localStorage.getItem('messages') ?? '[]'; <- podobnie do || ale tylko dla null i undefined
+const persistedMessagesString = localStorage.getItem('messages') || '[]';
+const messagesArray = JSON.parse(persistedMessagesString);
+
+const validateAuthorField = (authorValue) => {
+    if (!$authorErrorMessage)  {
+        alert('Cos poszlo nie tak!');
+        $authorInput.classList.add('error-border');
+        return false;
+    }
+    // Early return 
+    if (!authorValue) {
+        $authorErrorMessage.innerText = 'Pole wymagane!';
+        $authorInput.classList.add('error-border');
+        return false;
+    }
+
+    $authorInput.classList.remove('error-border');
+    $authorErrorMessage.innerText = '';
+
+    return true;
+}
+
+const validateMessageField = (messageValue) => {
+    if (!messageValue) {
+        document.getElementById('message-error').innerText = 'Pole wymagane!';
+        document.getElementById('message').classList.add('error-border');
+        return false;
+    } 
+
+    if (messageValue.length < 2) {
+        document.getElementById('message-error').innerText = 'Pole musi miec min 2 znaki!';
+        document.getElementById('message').classList.add('error-border');
+        return false;
+    }
+
+    $messageInput.classList.remove('error-border');
+    $messageErrorMessage.innerText = '';
+
+    return true;
+}
+
+const saveData = () => {
+    localStorage.setItem('messages', JSON.stringify(messagesArray))
+}
+
+class Message {
+    constructor(author, body, liked, disliked) {
+        this.author = author;
+        this.body = body;
+        this.liked = liked;
+        this.disliked = disliked;
+    }
+}
+
+const renderMesssages = (messagesArray) => {
+    $messageList.innerHTML = '';
+
+    for (const message of messagesArray) {
+        $messageList.innerHTML += `
+            <li class="list-group-item">
+                <div class="fw-bold">${message.author}</div>
+                <span>${message.body}</span>
+
+                <button class="like-btn btn btn-info" ${message.liked && 'disabled'}>:)</button>
+                <button class="dislike-btn btn btn-warning" ${message.disliked && 'disabled'}>:(</button>
+                <button class="delete-btn btn btn-danger">Usun</button>
+            </li>
+        `;
+    }
+
+    const likesBtn = Array.from(document.getElementsByClassName('like-btn'));
+
+    for (const likeBtn of likesBtn) {
+        likeBtn.addEventListener('click', (e) => {
+            e.target.setAttribute('disabled', true);
+
+            const $parentLi = e.target.parentElement;
+
+            // Znajdz wiadomosc, ktrej tresc pasuje do tresci z li, w ktorym jest klikniety przycisk 
+            const messageToLike = messagesArray.find(msg => {
+                return msg.body === $parentLi.querySelector('span').innerText;
+            });
+
+            messageToLike.liked = true;
+            saveData();
+        });
+    }
+
+    const dislikesBtn = Array.from(document.getElementsByClassName('dislike-btn'));
+
+    for (const dislikeBtn of dislikesBtn) {
+        dislikeBtn.addEventListener('click', (e) => {
+            e.target.setAttribute('disabled', true);
+        });
+    }
+
+    const deleteBtns = Array.from(document.getElementsByClassName('delete-btn'));
+
+    for (const deleteBtn of deleteBtns) {
+        deleteBtn.addEventListener('click', (e) => {
+            console.log('USUN')
+            const $parentLi = e.target.parentElement;
+
+            // Znajdz wiadomosc, ktorej tresc pasuje do tresci z li, w ktorym jest klikniety przycisk 
+            const indexOfMessageToDelete = messagesArray.findIndex(msg => {
+                return msg.body === $parentLi.querySelector('span').innerText;
+            });
+
+            messagesArray.splice(indexOfMessageToDelete, 1);
+
+            saveData();
+            renderMesssages(messagesArray);
+        });
+    }
+}
+
+document.querySelector('#message-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+
+    const author = formData.get('author');
+    const message = formData.get('message');
+
+    const isAuthorValid = validateAuthorField(author);
+    const isMessageValid = validateMessageField(message);
+
+    if (!isMessageValid) {
+        $messageInput.focus();
+    }
+    if (!isAuthorValid) {
+        $authorInput.focus();
+    }
+
+    if (!isAuthorValid || !isMessageValid) return;
+
+    messagesArray.push(new Message(author, message, false, false));
+
+    saveData();
+
+    renderMesssages(messagesArray);
+
+    defoultFormInput();
+});
+
+$authorInput.addEventListener('input', (e) => {
+    validateAuthorField(e.target.value);
+});
+
+$messageInput.addEventListener('input', (e) => {
+    validateMessageField(e.target.value);
+});
+
+
+
+renderMesssages(messagesArray);
